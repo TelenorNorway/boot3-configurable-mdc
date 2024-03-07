755ff3b293af11af7e4441e3a6ba3e257ea5a749
James Bradlee <james.bradlee@telenor.no>
2024-03-07 21:59:24 +0100

    fix: properly load MdcApplicationProperties

--- src/main/kotlin/no/telenor/kt/boot3/configurable/mdc/AutoConfigure.kt
+++ src/main/kotlin/no/telenor/kt/boot3/configurable/mdc/AutoConfigure.kt
@@ -1,8 +1,12 @@
 package no.telenor.kt.boot3.configurable.mdc
 
 import org.springframework.boot.autoconfigure.AutoConfiguration
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.ComponentScan
 
 @AutoConfiguration
 @ComponentScan
+@EnableConfigurationProperties(
+	MdcApplicationProperties::class,
+)
 internal class AutoConfigure
