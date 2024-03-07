f1158555476f3f294c26f47d72c104f69cc13c93
James Bradlee <james.bradlee@telenor.no>
2024-03-07 21:55:25 +0100

    fix: properly load configuration properties

--- src/main/kotlin/no/telenor/kt/boot3/configurable/mdc/MdcApplicationProperties.kt
+++ src/main/kotlin/no/telenor/kt/boot3/configurable/mdc/MdcApplicationProperties.kt
@@ -3,7 +3,5 @@ package no.telenor.kt.boot3.configurable.mdc
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Configuration
 
-@Suppress("ConfigurationProperties")
-@Configuration
 @ConfigurationProperties(prefix = "logging")
 internal data class MdcApplicationProperties(val map: Map<String, *>)
