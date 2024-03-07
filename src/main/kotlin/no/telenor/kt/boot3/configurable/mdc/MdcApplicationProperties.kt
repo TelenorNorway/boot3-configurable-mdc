17261289041351c8dfb859444f013dd7c50a7c06
James Bradlee <james.bradlee@telenor.no>
2024-03-07 21:59:43 +0100

    fix: remove unused import

--- src/main/kotlin/no/telenor/kt/boot3/configurable/mdc/MdcApplicationProperties.kt
+++ src/main/kotlin/no/telenor/kt/boot3/configurable/mdc/MdcApplicationProperties.kt
@@ -1,7 +1,6 @@
 package no.telenor.kt.boot3.configurable.mdc
 
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.context.annotation.Configuration
 
 @ConfigurationProperties(prefix = "logging")
 internal data class MdcApplicationProperties(val map: Map<String, *>)
