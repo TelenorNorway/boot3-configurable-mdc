8c6de7704080dc08f82147ab40c9306b547c2b03
James Bradlee <james.bradlee@telenor.no>
2024-06-10 11:53:48 +0200

    fix: AutoConfigure

--- src/main/kotlin/no/telenor/kt/boot3/configurable/mdc/AutoConfigure.kt
+++ src/main/kotlin/no/telenor/kt/boot3/configurable/mdc/AutoConfigure.kt
@@ -3,9 +3,15 @@ package no.telenor.kt.boot3.configurable.mdc
 import org.springframework.boot.autoconfigure.AutoConfiguration
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Import
 
 @AutoConfiguration
-@ComponentScan
+@Import(
+	MdcRequestContextFilter::class,
+	KeyRegistry::class,
+	MdcApplicationPropertiesProcessor::class,
+	MdcAppenderFilter::class,
+)
 @EnableConfigurationProperties(
 	MdcApplicationProperties::class,
 )
